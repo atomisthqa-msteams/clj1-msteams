89bb55b8545ed7ea8909a820e35cd80add3803fe
Jim Clark <slimslenderslacks@gmail.com>
2018-06-15 18:13:26 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,29 @@
+ (defproject clj1 "0.1.0-SNAPSHOT"
+   :description "IXME: write description"
+   :dependencies [[org.clojure/clojure "1.8.0"]
+                  [metosin/compojure-api "1.1.11"]]
+   :ring {:handler clj1.handler/app}
+   :jar-name "server.jar"
+   :target-path "target/%s/"
+   :repositories [["releases" {:url      "https://sforzando.artifactoryonline.com/sforzando/libs-release-local"
+                               :username [:gpg :env/artifactory_user]
+                               :password [:gpg :env/artifactory_pwd]}]
+                  ["plugins" {:url      "https://sforzando.artifactoryonline.com/sforzando/plugins-release"
+                              :username [:gpg :env/artifactory_user]
+                              :password [:gpg :env/artifactory_pwd]}]]
+   :container {:name       "pochta"
+               :dockerfile "/docker"
+               :hub        "sforzando-dockerv2-local.jfrog.io"}
+   :dynamodb-local {:port 6798
+                    :in-memory? true
+                    :shared-db? true}
+   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
+                    :plugins [[lein-ring "0.12.0"]
+                              [lein-metajar "0.1.1"]
+                              [lein-set-version "0.4.1"]
+                              [clj-plugin "0.6.2"]
+                              [lein-dynamodb-local "0.2.10"]]}
+              :metajar {:direct-linking true
+                        :aot :all
+                        :source-paths   ["env/prod/clj"]
+                        :resource-paths ["env/prod/resources"]}})
---
+++ src/clj1/handler.clj
@@ -0,0 +1,39 @@
+(ns clj1.handler
+  (:require [compojure.api.sweet :refer :all]
+            [ring.util.http-response :refer :all]
+            [schema.core :as s]))
+
+(s/defschema Pizza
+  {:name s/Str
+   (s/optional-key :description) s/Str
+   :size (s/enum :L :M :S)
+   :origin {:country (s/enum :FI :PO)
+            :city s/Str}})
+
+(defn get-pizza []
+  {:pizza :pie
+   :with :mushrooms})
+
+(def app
+  (api
+   {:swagger
+    {:ui "/"
+     :spec "/swagger.json"
+     :data {:info {:title "Clj1"
+                   :description "Compojure Api example"}
+            :tags [{:name "api", :description "some apis"}]}}}
+
+   (context "/api" []
+     :tags ["api"]
+
+     (GET "/plus" []
+       :return {:result Long}
+       :query-params [x :- Long, y :- Long]
+       :summary "adds two numbers together"
+       (ok {:result (+ x y)}))
+
+     (POST "/echo" []
+       :return Pizza
+       :body [pizza Pizza]
+       :summary "echoes a Pizza"
+       (ok (get-pizza))))))
