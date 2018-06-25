5b62b7bcde91070711149126c7d5f84dddb05a58
Jim Clark <slimslenderslacks@gmail.com>
2018-06-25 15:35:09 -0700

    .

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
  (defproject clj1 "0.1.0-SNAPSHOT"
    :description "IXME: write description"
-   :dependencies [[org.clojure/clojure "1.9.0"]
+   :dependencies [[org.clojure/clojure "1.8.0"]
                   [metosin/compojure-api "1.1.11"]]
    :ring {:handler clj1.handler/app}
    :jar-name "server.jar"
